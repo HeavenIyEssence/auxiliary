1ca08626301f2537010f10a0479eb32fc95225c8
HeavenIyEssence <54092635+HeavenIyEssence@users.noreply.github.com>
2020-07-15 15:21:25 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
     // status
-    client.user.setActivity(`${client.config.prefix}help | opensource`, { type: "PLAYING" });
+    client.user.setActivity(`${client.config.prefix}commands`, { type: "PLAYING" });
 
     // profile picture
     client.user.setAvatar(`${client.config.bot_picture}`);
