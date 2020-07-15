3421253cf4e704633c256e8bdc0584e43faa8514
HeavenIyEssence <54092635+HeavenIyEssence@users.noreply.github.com>
2020-07-15 15:24:21 -0700

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -24,17 +24,17 @@ exports.run = async (client, message, args) => {
 		.setTitle(`**${client.config.officer_role}: Commands**`)
 		.addField(`\`${client.config.prefix}add # username1, username2, etc...\``, `Adds # ${client.config.experience_name} to all profiles of provided usernames`)
 		.addField(`\`${client.config.prefix}remove # username1, username2, etc...\``, `Removes # ${client.config.experience_name} from all profiles of provided usernames`)
+		.addField(`\`${client.config.prefix}blacklist\``, `Blacklists a user from the group, which can only be executed by users with the HICOM role`)
 
 	// global cmds
 	var globalEmbed = new Discord.MessageEmbed()
 		.setColor(0x21ff7a)
 		.setTitle(`**Global: Commands**`)
-		.addField(`\`${client.config.prefix}information\``, `Pulls up information about the bot and sends a link to the repository over direct messages`)
 		.addField(`\`${client.config.prefix}commands\``, `Displays all of the commands`)
-		.addField(`\`${client.config.prefix}invite\``, `Displays a link for users to invite the bot into their servers`)
 		.addField(`\`${client.config.prefix}verify rblx_username\``, `Verifies your ROBLOX account (rblx_username) with your Discord account--simply makes sure you're not a robot`)
 		.addField(`\`${client.config.prefix}view rblx_username\``, `View the profile of the given username (rblx_username)`)
 		.addField(`\`${client.config.prefix}ranks\``, `View the XP requirements for all ranks found in the binded group`)
+		.addField(`\`${client.config.prefix}leaderboard\``, `Displays a list of the top 5 users with the most XP in the group`)
 
 	if (message.author.id === client.config.owner_id){
 		await message.author.send(ownerEmbed)
